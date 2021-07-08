f4d3f09ee3010633d2840e938bd0ee39d9b59891
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-07-08 16:27:48 +0100

    Created subpages for Profie page

---
+++ src/components/Account/Api/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Api = (props) => {
+  return <div>Api Account Profile</div>;
+};
+
+Api.propTypes = {};
+
+export default Api;
