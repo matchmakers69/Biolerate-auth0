10ca6ca0889e73e37f70e52bf0d4acd2d6b590ac
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-07-08 16:30:15 +0100

    Updated routesMapper component - added sub array routes for profile

--- src/config/routesMapper.js
+++ src/config/routesMapper.js
@@ -3,16 +3,26 @@ import { constants } from '../constants';
 
 import Feed from 'pages/Feed';
 
-const { PROFILE, FEED } = constants.route;
+const { PROFILE, FEED, PROFILE_GENERAL, PROFILE_API } = constants.route;
+
 const Profile = lazy(() => import('pages/Profile'));
+const GeneralProfle = lazy(() => import('components/Account/General'));
+const GeneralApi = lazy(() => import('components/Account/Api'));
 
 const routes = [
   {
     path: PROFILE,
     name: 'Profile',
     component: Profile,
-    exact: true,
     isPrivate: true,
+    routes: [
+      { path: PROFILE_GENERAL, component: GeneralProfle, isPrivate: true },
+      {
+        path: PROFILE_API,
+        component: GeneralApi,
+        isPrivate: true,
+      },
+    ],
   },
   {
     path: FEED,
